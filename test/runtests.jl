7870701397bcc55d9427bb5b82ee2a2c2ad29d03
Tim Holy <tim.holy@gmail.com>
2016-09-25 06:08:23 -0500

    Test for ambiguities

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,9 @@
+using FixedPointNumbers, Base.Test
+
+if VERSION >= v"0.5.0"
+    @test isempty(detect_ambiguities(FixedPointNumbers, Base, Core))
+end
+
 for f in ["ufixed.jl", "fixed.jl"]
     println("Testing $f")
     include(f)

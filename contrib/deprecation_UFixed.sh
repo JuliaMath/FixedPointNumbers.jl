fdf550693357a55820be2c6f0fae6b5a43ff38b9
Tim Holy <tim.holy@gmail.com>
2016-10-11 13:31:17 -0500

    Add a shell script to aid in fixing deprecations

---
+++ contrib/deprecation_UFixed.sh
@@ -0,0 +1,8 @@
+sed -i "s/UFixed8/N0f8/g" $*
+sed -i "s/UFixed10/N6f10/g" $*
+sed -i "s/UFixed12/N4f12/g" $*
+sed -i "s/UFixed14/N2f14/g" $*
+sed -i "s/UFixed16/N0f16/g" $*
+# For types from ColorTypes
+sed -i "s/U8/N0f8/g" $*
+sed -i "s/U16/N0f16/g" $*

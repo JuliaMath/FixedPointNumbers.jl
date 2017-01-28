575ed54fca0dc83c91d984c00c954b27b8cf0ae7
Tim Holy <tim.holy@gmail.com>
2017-01-28 05:20:39 -0600

    Update deprecation_UFixed shell script

--- contrib/deprecation_UFixed.sh
+++ contrib/deprecation_UFixed.sh
@@ -3,6 +3,7 @@ sed -i "s/UFixed10/N6f10/g" $*
 sed -i "s/UFixed12/N4f12/g" $*
 sed -i "s/UFixed14/N2f14/g" $*
 sed -i "s/UFixed16/N0f16/g" $*
+sed -i "s/UFixed/Normed/g" $*
 # For types from ColorTypes
 sed -i "s/U8/N0f8/g" $*
 sed -i "s/U16/N0f16/g" $*

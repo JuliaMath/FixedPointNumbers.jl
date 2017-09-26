fd8e06cd2683c4fff5edf17963c79118e785c58e
Tim Holy <tim.holy@gmail.com>
2017-09-26 04:59:47 -0500

    Prevent overflow when converting from Float16

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,8 @@
 using FixedPointNumbers, Base.Test
 
+@test isempty(detect_ambiguities(FixedPointNumbers, Base, Core))
+
 for f in ["normed.jl", "fixed.jl"]
     println("Testing $f")
     include(f)
 end
-
-@test isempty(detect_ambiguities(FixedPointNumbers, Base, Core))

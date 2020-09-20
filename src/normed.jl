b126c1b5231589fd01ffcc7c9e8884c95061f498
kimikage <kimikage.ceo@gmail.com>
2020-09-20 16:55:11 +0900

    Remove old iterator methods (#231)
    This removes `start`, `next` and `done`.
    They are no longer used in Julia v1 and were left as private methods of `FixedPointNumbers`.

--- src/normed.jl
+++ src/normed.jl
@@ -316,16 +316,6 @@ function round(::Type{Ti}, x::Normed) where {Ti <: Integer}
     convert(Ti, r > (rawone(x) >> 0x1) ? d + oneunit(rawtype(x)) : d)
 end
 
-# Iteration
-# The main subtlety here is that iterating over N0f8(0):N0f8(1) will wrap around
-# unless we iterate using a wider type
-@inline start(r::StepRange{T}) where {T <: Normed} = widen1(reinterpret(r.start))
-@inline next(r::StepRange{T}, i::Integer) where {T <: Normed} = (T(i,0), i+reinterpret(r.step))
-@inline function done(r::StepRange{T}, i::Integer) where {T <: Normed}
-    i1, i2 = reinterpret(r.start), reinterpret(r.stop)
-    isempty(r) | (i < min(i1, i2)) | (i > max(i1, i2))
-end
-
 function decompose(x::Normed)
     g = gcd(reinterpret(x), rawone(x))
     div(reinterpret(x),g), 0, div(rawone(x),g)

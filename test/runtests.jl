3d371dccc6372269424db81e85268b7c63eeb446
kimikage <kimikage.ceo@gmail.com>
2020-12-19 11:56:31 +0900

    Use GitHub Actions for unit testing on Arm arch. (#238)
    The test runs on a docker container with QEMU.
    This also updates TagBot.yml.

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,8 +2,10 @@ using FixedPointNumbers, Test
 
 @test isempty(detect_ambiguities(FixedPointNumbers, Base, Core))
 
-using Documenter
-doctest(FixedPointNumbers, manual = false)
+if Sys.ARCH === :x86_64 || Sys.ARCH === :i686
+    using Documenter
+    doctest(FixedPointNumbers, manual = false)
+end
 
 @testset "normed" begin
     include("normed.jl")

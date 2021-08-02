dbd18702599cce15a01c97799fea41bc126f7efc
Kristoffer Carlsson <kricarl@student.chalmers.se>
2021-08-02 20:34:02 +0200

    fix tests on 1.7 (#253)
    Co-authored-by: Johnny Chen <johnnychen94@hotmail.com>
    Co-authored-by: kimikage <kimikage.ceo@gmail.com>

--- test/fixed.jl
+++ test/fixed.jl
@@ -228,7 +228,7 @@ end
     @test length(r) == 256
     QInt1 = Fixed{Int,1}
     @test length(QInt1(0):eps(QInt1):typemax(QInt1)-eps(QInt1)) == typemax(Int)
-    @test Base.unsafe_length(typemin(QInt1):eps(QInt1):typemax(QInt1)-eps(QInt1)) == -1
+    @test_throws OverflowError length(typemin(QInt1):eps(QInt1):typemax(QInt1)-eps(QInt1))
     @test_throws OverflowError length(QInt1(-1):eps(QInt1):typemax(QInt1)-eps(QInt1))
 end
 

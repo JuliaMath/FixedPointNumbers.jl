841f8b1abb50b25a91c72df8d0dcc5e74d4a39d1
Yuto Horikawa <hyrodium@gmail.com>
2024-04-13 20:58:56 +0900

    Enable package extension for Statistics (#277)

--- src/FixedPointNumbers.jl
+++ src/FixedPointNumbers.jl
@@ -9,7 +9,6 @@ import Base: ==, <, <=, -, +, *, /, ~, isapprox,
              signed, unsigned, copysign, flipsign, signbit,
              length
 
-import Statistics   # for _mean_promote
 import Random: Random, AbstractRNG, SamplerType, rand!
 
 import Base.Checked: checked_mul, checked_div
@@ -350,10 +349,6 @@ Base.mul_prod(x::FixedPoint, y::FixedPoint) = Treduce(x) * Treduce(y)
 Base.reduce_empty(::typeof(Base.mul_prod), ::Type{F}) where {F<:FixedPoint} = one(Treduce)
 Base.reduce_first(::typeof(Base.mul_prod), x::FixedPoint)  = Treduce(x)
 
-if isdefined(Statistics, :_mean_promote)
-    Statistics._mean_promote(x::Real, y::FixedPoint) = Treduce(y)
-end
-
 """
     sd, ad = scaledual(s::Number, a)
 
@@ -404,6 +399,10 @@ function rand!(r::AbstractRNG, A::Array{X}, ::SamplerType{X}) where {T, X <: Fix
     At = unsafe_wrap(Array, reinterpret(Ptr{T}, pointer(A)), size(A))
     Random.rand!(r, At, SamplerType{T}())
     A
+end
+
+if !isdefined(Base, :get_extension)
+    include("../ext/FixedPointNumbersStatisticsExt.jl")
 end
 
 if VERSION >= v"1.1" # work around https://github.com/JuliaLang/julia/issues/34121
